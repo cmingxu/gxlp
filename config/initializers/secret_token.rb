74742f181034582d648e36a9fb75487ce86f4f5c
cming <cming.xu@photomugs.com>
2011-05-19 20:20:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gxlp::Application.config.secret_token = 'cd16f960ef5bb58c17825a264b018da7c14ff78d907a4dc31b3b326e5e2fb08e2702cee504569c95b7290c72b4047b04eaddbd2011ea2a4520e11def3a324cf6'
